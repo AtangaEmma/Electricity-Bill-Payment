d28fcbc4d1c3ca45f49576a732b723e809ba20fa
AtangaEmma <atango.infotech@gmail.com>
2025-01-10 15:47:03 +0100

    Update ElectricityBillApplication.java

--- scr/main/main-class/ElectricityBillApplication.java
+++ scr/main/main-class/ElectricityBillApplication.java
@@ -3,6 +3,7 @@ package com.example.electricitybill;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringApplication
 @SpringBootApplication
 public class ElectricityBillApplication {
     public static void main(String[] args) {
