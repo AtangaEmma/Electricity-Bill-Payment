e35b6f74fbd7712d9990743efadf7685a8ceb71f
AtangaEmma <atango.infotech@gmail.com>
2025-01-10 16:46:42 +0100

    Create ElectricityBillApplication.java

---
+++ ElectricityBillApplication.java
@@ -0,0 +1,12 @@
+package com.example.electricitybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringApplication
+@SpringBootApplication
+public class ElectricityBillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElectricityBillApplication.class, args);
+    }
+}
